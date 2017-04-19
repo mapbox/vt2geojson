e857da788d117d57ba546cfd354de8a8c301eec8
amishas157 <amishas157@gmail.com>
2017-04-19 12:50:18 +0530

    Add test for checking if undefined layer present

--- test.js
+++ test.js
@@ -5,9 +5,13 @@ var nock = require('nock');
 var fs = require('fs');
 var vt2geojson = require('./');
 
-var tile = nock('http://api.mapbox.com')
+var tile1 = nock('http://api.mapbox.com')
     .get('/9/150/194.mvt')
     .reply(200, fs.readFileSync('fixtures/9-150-194.mvt'));
+
+var tile2 = nock('http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7')
+    .get('/16/46886/30383.mvt')
+    .reply(200, fs.readFileSync('fixtures/16-46886-30383.mvt'));
 
 test('fails without uri', function (t) {
     vt2geojson({}, function (err, result) {
@@ -45,6 +49,19 @@ test('url', function (t) {
         t.end();
     });
 });
+
+test('undefined layer', function (t) {
+    vt2geojson({
+        uri: 'http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v7/16/46886/30383.mvt',
+        layer: 'water'
+    }, function (err, result) {
+        t.ifError(err);
+        t.deepEqual(result.type, 'FeatureCollection');
+        t.deepEqual(result.features.length, 0);
+        t.end();
+    });
+});
+
 
 test('local file: relative', function (t) {
     vt2geojson({
