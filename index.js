d2cd63382b8093686d55ec227d562591c16b3647
Jesse Crocker <Jesse@datamongers.net>
2017-04-21 09:53:23 -0700

    Include layer in properties if there are multiple layers (#6)

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ function readTile(args, buffer, callback) {
             for (var i = 0; i < layer.length; i++) {
                 var feature = layer.feature(i).toGeoJSON(args.x, args.y, args.z);
                 feature.coordinates = layer.feature(i).loadGeometry();
+                if (layers.length > 1) feature.properties.layer = layerID;
                 collection.features.push(feature);
             } 
         }
